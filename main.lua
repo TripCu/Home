f6ff51cc7e437be87dba9624fbb921a91645f9ee
TripCu <119246941+TripCu@users.noreply.github.com>
2022-12-01 22:23:17 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,3 @@
---[[
-    GD50 2018
-    Pong Remake
-    pong-0
-    "The Day-0 Update"
-    -- Main Program --
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
-]]
-
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
